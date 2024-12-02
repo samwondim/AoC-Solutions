468baf9a59bc1c36f0d45afde27229e56e60569e
samwondim <samwondim@gmail.com>
2024-12-02 20:50:50 +0300

    chore: add solution to part two

--- 2024/TypeScript/day2/day2.ts
+++ 2024/TypeScript/day2/day2.ts
@@ -22,8 +22,28 @@ const partOne = (fileName: string) => {
   console.log(reports.filter(isSafe).length);
 }
 
+const isSafeII = (report: string): boolean => {
+  const levels = report.split(" ").map(Number);
+
+  if (isSafe(report)) return true;
+  for (let i = 0; i < levels.length; i++) {
+    const modedLevels = [...levels.slice(0, i), ...levels.slice(i + 1)];
+    const modedReport = modedLevels.join(" ");
+
+    if (isSafe(modedReport)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 const partTwo = (fileName: string) => {
+  const contents: string = fs.readFileSync(fileName, 'utf8');
+  const reports = contents.trim().split("\n");
+
+  console.log(reports.filter(isSafeII).length)
 
 }
-partOne("part_one.txt");
-// partTwo("part_two.txt");
+// partOne("part_one.txt");
+partTwo("part_two.txt");
